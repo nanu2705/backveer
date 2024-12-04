a69f8ac1aed7ffb27b15fe93b9ed07c81ea456e4
nanu2705 <nandanipatel057@gmail.com>
2024-12-04 16:45:02 +0530

    backveer

--- routes/reviewroute.js
+++ routes/reviewroute.js
@@ -6,29 +6,38 @@ const app = express();
 
 
 app.post('/reviews', (req, res) => {
-    const newReview = req.body; 
 
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading reviews:', err);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
-        try {
-            const reviews = JSON.parse(data);
-            reviews.push(newReview); 
+    const { name, initialName, review, rating } = req.body;
 
-            fs.writeFile(filePath, JSON.stringify(reviews, null, 2), 'utf8', (writeErr) => {
-                if (writeErr) {
-                    console.error('Error writing to reviews file:', writeErr);
-                    return res.status(500).json({ error: 'Internal Server Error' });
-                }
-                return res.json({ success: true, message: 'Review added successfully' });
-            });
-        } catch (parseError) {
-            console.error('Error parsing reviews:', parseError);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
-    });
+       
+
+        console.log(name, initialName, review, rating)
+        res.json({ success: true, message: 'Thanks for your inquiry' });
+
+
+    // const newReview = req.body; 
+
+    // fs.readFile(filePath, 'utf8', (err, data) => {
+    //     if (err) {
+    //         console.error('Error reading reviews:', err);
+    //         return res.status(500).json({ error: 'Internal Server Error' });
+    //     }
+    //     try {
+    //         const reviews = JSON.parse(data);
+    //         reviews.push(newReview); 
+
+    //         fs.writeFile(filePath, JSON.stringify(reviews, null, 2), 'utf8', (writeErr) => {
+    //             if (writeErr) {
+    //                 console.error('Error writing to reviews file:', writeErr);
+    //                 return res.status(500).json({ error: 'Internal Server Error' });
+    //             }
+    //             return res.json({ success: true, message: 'Review added successfully' });
+    //         });
+    //     } catch (parseError) {
+    //         console.error('Error parsing reviews:', parseError);
+    //         return res.status(500).json({ error: 'Internal Server Error' });
+    //     }
+    // });
 });
 
 export default app;
